af371266254068fd25a9aefbcd4d08b37de1d599
RyanVaughan <ryan.vaughan@okstate.edu>
2023-01-23 10:17:13 -0600

    adding test case to show that the algorithm is unable to pivot

--- lufactor/tests/test_examples.py
+++ lufactor/tests/test_examples.py
@@ -16,6 +16,20 @@ def test_lu_crout_2x2():
     isequal = np.all(np.isclose(Aprime, A))
     assert isequal
 
+def test_lu_crout_2x2_pivot_needed():
+    A = np.array([[1.0e-16, 1.0],
+                  [    2.0, 1.0]])
+
+    Q = lu_crout(A)
+
+    # decompose Q into LU
+    L = Q - np.triu(Q, k=1)
+    U = np.triu(Q, k=1) + np.identity(np.shape(Q)[0])
+
+    # multiply LU and check if equal to A
+    Aprime = np.matmul(L,U)
+    isequal = np.all(np.isclose(Aprime, A))
+    assert isequal
 
 
 def test_lu_crout_4x4():
